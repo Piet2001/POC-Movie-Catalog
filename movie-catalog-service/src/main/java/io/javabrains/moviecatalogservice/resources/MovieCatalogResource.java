71778416218cd75c77b6ac9cc90b6b8c06a0f8a4
Twan <piet2001@fireman.net>
2021-04-07 15:06:08 +0200

    add userratings

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -28,13 +30,17 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
+        return ratings.getUserRating().stream().map(rating -> {
+            //get al rated movie ID's
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            // foreach movie call movie info service and ger details
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
+
              /*
              Movie movie = webClientBuilder.build()
                     .get()
@@ -43,12 +49,3 @@ public class MovieCatalogResource {
                     .bodyToMono(Movie.class)
                     .block();
             */
-
-            return new CatalogItem(movie.getName(), "Test", rating.getRating());
-        }).collect(Collectors.toList());
-
-        //get al rated movie ID's
-
-        // foreach movie call movie info service and ger details
-    }
-}
